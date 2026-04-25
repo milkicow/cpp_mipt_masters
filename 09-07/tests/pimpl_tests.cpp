7861476572a3873c0d8cb607dbc285cd65473d8d
milkicow <kozlov.aleksandr@phystech.edu>
2026-04-25 20:47:09 +0300

    Fixup 09-07

--- 09-07/tests/pimpl_tests.cpp
+++ 09-07/tests/pimpl_tests.cpp
@@ -1,25 +1,26 @@
 #include <gtest/gtest.h>
 
+#include "impl.hpp"
 #include "pimpl.hpp"
 
 TEST(PimplTest, Construction) {
     Entity a;
-    ASSERT_EQ(a.test(), 0x67);
+    ASSERT_EQ(a.get()->test(), 0x67);
 }
 
 TEST(PimplTest, MoveConstruction) {
     Entity a;
-    ASSERT_EQ(a.test(), 0x67);
+    ASSERT_EQ(a.get()->test(), 0x67);
 
     Entity b(std::move(a));
-    ASSERT_EQ(b.test(), 0x67);
+    ASSERT_EQ(b.get()->test(), 0x67);
 }
 
 TEST(PimplTest, MoveAssignment) {
     Entity a;
-    ASSERT_EQ(a.test(), 0x67);
+    ASSERT_EQ(a.get()->test(), 0x67);
 
     Entity c;
     c = std::move(a);
-    ASSERT_EQ(c.test(), 0x67);
+    ASSERT_EQ(c.get()->test(), 0x67);
 }
